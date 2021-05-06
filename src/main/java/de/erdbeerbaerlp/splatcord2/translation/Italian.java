03b79b6120464681cb8f5ba6015bb920f89ebe74
Monty <monty.github@gmail.com>
2021-05-06 21:18:10 +0200

    Misc changes to the Italian localization

--- src/main/java/de/erdbeerbaerlp/splatcord2/translation/Italian.java
+++ src/main/java/de/erdbeerbaerlp/splatcord2/translation/Italian.java
@@ -6,8 +6,8 @@ public class Italian extends EnglishBase {
     {
         salmonStage = "Arene:";
         weapons = "Armi:";
-        stagesTitle = "Arene attuali";
-        futureStagesTitle = "Arene future";
+        stagesTitle = "Arene correnti";
+        futureStagesTitle = "Prossime arene";
         footer_ends = "Termina";
         footer_closed = "Chiuso";
         unknownCommand = "Comando sconosciuto";
@@ -18,20 +18,20 @@ public class Italian extends EnglishBase {
         helpMessage = "Comandi amministratore:__\n" +
                 Config.instance().discord.prefix + "setlang <de|en|it> - Cambia la lingua del bot per questo server\n" +
                 Config.instance().discord.prefix + "setsalmon - Seleziona questo canale per le notifiche delle Salmon Run\n" +
-                Config.instance().discord.prefix + "delsalmon - Elimina il canale per le notifiche delle Salmon Run correntemente impostato\n" +
+                Config.instance().discord.prefix + "delsalmon - Deseleziona il canale impostato per le notifiche delle Salmon Run\n" +
                 Config.instance().discord.prefix + "setstage - Seleziona questo canale per le notifiche delle arene\n" +
-                Config.instance().discord.prefix + "delstage - Elimina il canale per le notifiche delle arene correntemente impostato\n\n" +
+                Config.instance().discord.prefix + "delstage - Deseleziona il canale impostato per le notifiche delle arene\n\n" +
                 "__Comandi utente:__\n" +
                 Config.instance().discord.prefix + "code - Genera un codice casuale per una battaglia privata\n" +
-                Config.instance().discord.prefix + "invite - Manda l'URL invito per questo bot\n" +
-                Config.instance().discord.prefix + "support - Manda l'URL invito per il server discord del bot\n" +
-                Config.instance().discord.prefix + "stages - Manda le arene attualmente attive\n" +
-                Config.instance().discord.prefix + "rotation - Manda le arene attualmente attive più le due rotazioni successive\n" +
-                Config.instance().discord.prefix + "salmon - Manda le Salmon Run correnti o successive\n" +
-                Config.instance().discord.prefix + "splatnet - Manda cosa è correntemente disponibile su Calama-zone, alias: `sn`, `gear`\n";
+                Config.instance().discord.prefix + "invite - Invia l'invito per questo bot\n" +
+                Config.instance().discord.prefix + "support - Invia l'invito per il server discord del bot\n" +
+                Config.instance().discord.prefix + "stages - Invia le arene correnti\n" +
+                Config.instance().discord.prefix + "rotation - Invia le arene correnti più le due rotazioni successive\n" +
+                Config.instance().discord.prefix + "salmon - Invia le Salmon Run correnti o successive\n" +
+                Config.instance().discord.prefix + "splatnet - Invia cosa è correntemente disponibile su Calama-zone, alias: `sn`, `gear`\n";
         unknownLanguage = "Non hai specificato una lingua valida. Lingue valide: tedesco (de), inglese (en), e italiano (it)";
         deleteSuccessful = "Eliminato correttamente";
-        noWritePerms = "Questo bot non ha i permessi necessari per scrivere in questo canale";
+        noWritePerms = "Questo bot non ha i permessi necessari per inviare messaggi in questo canale";
         skill = "Abilità:";
         skillSlots = "Slot abilità:";
         price = "Prezzo:";
