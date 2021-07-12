72569889dc888bf90e1c6d074aef3f1d4de033a9
ErdbeerbaerLP <27149563+ErdbeerbaerLP@users.noreply.github.com>
2021-07-12 17:51:07 +0200

    Update setlang command to use predefined choices

--- README.md
+++ README.md
@@ -1,13 +1,15 @@
 Splatcord 2
 ==
+[![Discord Bots](https://top.gg/api/widget/status/822228767165644872.svg)](https://top.gg/bot/822228767165644872)
+
 
 Splatcord 2 is an Discord bot providing Splatoon 2 stage and salmon run notifications
 
-To enable stage notifications, use `s!setstage` in the destination channel.
-To enable SalmonRun notifications, use `s!setsalmon` in the target channel.
-To disable them, you can use `s!delsalmon` or `s!delstage` in any channel.
+To enable stage notifications, use `/setstage` in the destination channel.
+To enable SalmonRun notifications, use `/setsalmon` in the target channel.
+To disable them, you can use `/delsalmon` or `/delstage` in any channel.
 
-You can also change language, to do that you use `s!setlang <language>`
+You can also change language, to do that you use `/setlang <language>`
 
 Supported languages are:
 - english (en)
--- src/main/java/de/erdbeerbaerlp/splatcord2/commands/SetlangCommand.java
+++ src/main/java/de/erdbeerbaerlp/splatcord2/commands/SetlangCommand.java
@@ -6,15 +6,25 @@ import de.erdbeerbaerlp.splatcord2.storage.BotLanguage;
 import de.erdbeerbaerlp.splatcord2.storage.CommandRegistry;
 import de.erdbeerbaerlp.splatcord2.storage.json.translations.Locale;
 import net.dv8tion.jda.api.events.interaction.SlashCommandEvent;
+import net.dv8tion.jda.api.interactions.commands.Command;
 import net.dv8tion.jda.api.interactions.commands.OptionMapping;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
+import net.dv8tion.jda.api.interactions.commands.build.OptionData;
 
+import java.awt.*;
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class SetlangCommand extends BaseCommand {
     public SetlangCommand(Locale l) {
         super("setlang", l.botLocale.cmdSetlangDesc);
-        addOption(OptionType.STRING, "language", Arrays.toString(BotLanguage.values()));
+        final OptionData option = new OptionData(OptionType.STRING, "language", Arrays.toString(BotLanguage.values()),true);
+        final ArrayList<Command.Choice> languages = new ArrayList<>();
+        for(BotLanguage lang : BotLanguage.values()){
+            languages.add(new Command.Choice(lang.name().toLowerCase(),lang.val));
+        }
+        option.addChoices(languages);
+        addOptions(option);
 
     }
 
@@ -35,33 +45,10 @@ public class SetlangCommand extends BaseCommand {
             if (langOption == null) {
                 ih.editOriginal(lang[0].botLocale.unknownLanguage).queue();
             }else{
-                switch (langOption.getAsString().toLowerCase()) {
-                    case "deutsch":
-                    case "german":
-                    case "de":
-                        Main.iface.setServerLang(ev.getGuild().getIdLong(), BotLanguage.GERMAN);
-                        lang[0] = Main.translations.get(Main.iface.getServerLang(ev.getGuild().getIdLong()));
-                        ih.editOriginal(lang[0].botLocale.languageSetMessage).queue();
-                        CommandRegistry.setCommands(ev.getGuild());
-                        break;
-                    case "english":
-                    case "englisch":
-                    case "en":
-                        Main.iface.setServerLang(ev.getGuild().getIdLong(), BotLanguage.ENGLISH);
-                        lang[0] = Main.translations.get(Main.iface.getServerLang(ev.getGuild().getIdLong()));
-                        ih.editOriginal(lang[0].botLocale.languageSetMessage).queue();
-                        CommandRegistry.setCommands(ev.getGuild());
-                        break;
-                    case "italian":
-                    case "it":
-                        Main.iface.setServerLang(ev.getGuild().getIdLong(), BotLanguage.ITALIAN);
-                        lang[0] = Main.translations.get(Main.iface.getServerLang(ev.getGuild().getIdLong()));
-                        ih.editOriginal(lang[0].botLocale.languageSetMessage).queue();
-                        CommandRegistry.setCommands(ev.getGuild());
-                        break;
-                    default:
-                        ih.editOriginal(lang[0].botLocale.unknownLanguage).queue();
-                }
+                Main.iface.setServerLang(ev.getGuild().getIdLong(), BotLanguage.fromInt(Integer.parseInt(langOption.getAsString())));
+                lang[0] = Main.translations.get(Main.iface.getServerLang(ev.getGuild().getIdLong()));
+                ih.editOriginal(lang[0].botLocale.languageSetMessage).queue();
+                CommandRegistry.setCommands(ev.getGuild());
             }
         });
 
